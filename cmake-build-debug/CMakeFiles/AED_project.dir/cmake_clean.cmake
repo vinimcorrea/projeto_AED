d8f8b812d84c3ff3c4bbfcddcd263fc71c3040b5
Vinicius <vinimcorrea@hotmail.com>
2021-12-01 16:13:51 +0000

    commited first files

---
+++ cmake-build-debug/CMakeFiles/AED_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AED_project"
+  "AED_project.pdb"
+  "CMakeFiles/AED_project.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AED_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
